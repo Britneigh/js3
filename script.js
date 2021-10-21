d12113d0079ecf6b038d16490d34f930cddd5c44
Britneigh <britneynguyen18@yahoo.com>
2021-10-21 09:41:13 +0100

    Finished all 3 tasks

---
+++ script.js
@@ -0,0 +1,29 @@
+// Task 1
+let number = prompt("Enter a number:", "")
+// Taking user input
+
+if (number % 2 == 0){
+    alert ("Even number")
+}else{
+    alert ("Odd number")
+}
+
+// Task 2
+let number1 = prompt("Enter first number:", "")
+let number2 = prompt("Enter second number:", "")
+// Taking user input
+
+if (number1 % number2 == 0){
+    alert ("True")
+}else{
+    alert ("False")
+}
+
+// Task 3
+let students = 344
+let buses = 42
+let answer = Math.ceil (students / buses)
+alert ("The number of school buses needed:" + answer)
+/* Shorter way:
+ alert(Math.ceil(344/42))
+*/
